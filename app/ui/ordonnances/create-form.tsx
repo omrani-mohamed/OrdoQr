ceb9bc06cb0958fa24f364bd3904df54d384dfab
omrani-mohamed <omrani.mohamedamine@esprit.tn>
2024-07-11 11:47:07 +0100

    creating perscription form

--- app/ui/ordonnances/create-form.tsx
+++ app/ui/ordonnances/create-form.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import {UserCircleIcon} from '@heroicons/react/24/outline';
 import Search from '../search';
-
+import { Button } from '../medecin/button';
+import { useState } from 'react';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 const calculateAge = (dateOfBirth: string): number => {
     const [day, month, year] = dateOfBirth.split('/');
@@ -13,6 +17,13 @@ const calculateAge = (dateOfBirth: string): number => {
       age--;
     }
     return age;
+};
+
+type Medication = {
+  name: string;
+  type: string;
+  dosage: number;
+  comment: string;
 };
 
 type Patient = {
@@ -29,13 +40,31 @@ const patient4: Patient = { id: 4, name: 'Bob Brown',dateOfBirth: "01/10/1996",g
 const Patients: Patient[] = [patient1, patient2, patient3, patient4];
 
 export default function CreatePerscriptionForm() {
+
+  const [medications, setMedications] = useState<Medication[]>([
+    { name: '', type: '', dosage: 0, comment: '' }
+  ]);
+
+  const handleAddMedication = () => {
+    setMedications([...medications, { name: '', type: '', dosage: 0, comment: '' }]);
+  };
+
+  const handleMedicationChange = (index: number, field: string, value: string | number) => {
+    const newMedications = [...medications];
+    newMedications[index] = { ...newMedications[index], [field]: value };
+    setMedications(newMedications);
+  };
+
+  const handleRemoveMedication = (index: number) => {
+    setMedications(medications.filter((_, i) => i !== index));
+  };
     return (
       <form>
-        <div className="rounded-md bg-gray-50 p-4 md:p-6">
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Patient's Name */}
         <div className="mb-4">
           <label htmlFor="Patient" className="mb-2 block text-sm font-medium">
-            choisir le patient
+            Choisir le patient
           </label>
           <div className="relative">
             <select
@@ -47,10 +76,10 @@ export default function CreatePerscriptionForm() {
             >
               <option value="" disabled>
                 Selectionner un patient
-                </option>
+              </option>
               {Patients.map((Patient) => (
                 <option key={Patient.id} value={Patient.id}>
-                  {Patient.gender === "Homme" ? "M " : "Mme "} 
+                  {Patient.gender === "Homme" ? "M " : "Mme "}
                   {Patient.name}
                   {"              "}
                   {`age : ${calculateAge(Patient.dateOfBirth)}`}
@@ -64,60 +93,83 @@ export default function CreatePerscriptionForm() {
         </div>
         <div className="mb-4">
           <label htmlFor="taille" className="mb-2 block text-sm font-medium">
-            rechercher le medicament ici
+            Rechercher le medicament ici
           </label>
-          <Search placeholder='Nom du medicament'/>
+          <Search placeholder="Nom du medicament" />
         </div>
         <br />
         <br />
         <div className="mb-4">
           <label htmlFor="taille" className="mb-2 block text-sm font-medium">
-            Dans le cas ou le medicament est introuvable, insérez manuellement  
+            Dans le cas où le medicament est introuvable, insérez manuellement
           </label>
-          <div className="mb-4 flex lg:flex-row sm:flex-col max-sm:flex-col">
-                        <div className="relative lg:mr-8 sm:mr-0 lg:w-1/2 sm:w-auto sm:mb-4">
-                            <label className="mb-2 block text-sm font-medium ">
-                                Nom Medicament: 
-                            </label>
-                            <input
-                            id="address"
-                            name="address"
-                            type="text"
-                            className="block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
-                            >
-                            </input>
-                        </div>
-                        <div className="relative lg:mr-8 sm:mr-0 sm:mb-4 ">
-                            <label className="mb-2 block text-sm font-medium">
-                                type : 
-                            </label>
-                            <input
-                            id="ville"
-                            name="ville"
-                            type="text"
-                            className="block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
-                            >
-                            </input>
-                        </div>
-                        <div className="relative sm:mr-0 sm:mb-4">
-                            <label className="mb-2 block text-sm font-medium">
-                                Dosage : 
-                            </label>
-                            <input
-                            id="code"
-                            name="code"
-                            type="number"
-                            className="block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
-                            >
-                            </input>
-                        </div>
-                    </div>
-                    <div className="flex flex-col">
-                    <label htmlFor="comment">Commentaire additionnel</label>
-                    <textarea name="comment" id="commentID"><p className='ml-4'></p></textarea>
-                    </div>
+          {medications.map((medication, index) => (
+            <div key={index} className="mb-4 flex lg:flex-row sm:flex-col max-sm:flex-col">
+              <div className="relative lg:mr-4 sm:mr-0 lg:w-1/3 sm:w-auto sm:mb-4">
+                <label className="mb-2 block text-sm font-medium">
+                  Nom Medicament:
+                </label>
+                <input
+                  type="text"
+                  className="block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
+                  value={medication.name}
+                  onChange={(e) => handleMedicationChange(index, 'name', e.target.value)}
+                />
+              </div>
+              <div className="relative lg:mr-4 sm:mr-0 sm:mb-4">
+                <label className="mb-2 block text-sm font-medium">
+                  Type:
+                </label>
+                <input
+                  type="text"
+                  className="block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
+                  value={medication.type}
+                  onChange={(e) => handleMedicationChange(index, 'type', e.target.value)}
+                />
+              </div>
+              <div className="flex-1 lg:mr-4 sm:mr-0 sm:mb-4">
+                <label className="mb-2 block text-sm font-medium">
+                  Dosage:
+                </label>
+                <input
+                  type="number"
+                  className="block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
+                  value={medication.dosage}
+                  onChange={(e) => handleMedicationChange(index, 'dosage', Number(e.target.value))}
+                />
+              </div>
+              <div className="flex flex-col mr-4">
+                <label htmlFor="comment" className="smb-2 block text-sm font-medium">
+                  Commentaire additionnel:
+                </label>
+                <textarea
+                  className="block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
+                  value={medication.comment}
+                  onChange={(e) => handleMedicationChange(index, 'comment', e.target.value)}
+                />
+              </div>
+              <button
+                type="button"
+                className="mt-6 mb-6 inline-flex items-center border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700"
+                onClick={() => handleRemoveMedication(index)}
+              >
+              <DeleteOutlinedIcon/>
+              </button>
+            </div>
+          ))}
+          <button
+            type="button"
+            className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700"
+            onClick={handleAddMedication}
+          >
+            Ajouter Medicament
+          </button>
         </div>
+        <div className="mt-6 flex justify-end gap-4">
+          <Button>Enregistrer</Button>
+          <Button className='bg-green-500'>Submit</Button>
         </div>
+      </div>
       </form>
     );
   }
