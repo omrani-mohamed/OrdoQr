aeba55bf1848d53eb92c23dbc3cd95b0c5078a58
omrani-mohamed <omrani.mohamedamine@esprit.tn>
2024-07-24 16:38:04 +0100

    trouble with environnement variables

--- app/api/updatePatient/route.ts
+++ app/api/updatePatient/route.ts
@@ -5,7 +5,6 @@ import { Patient } from "@/model/patient-model";
 export const POST = async (req: Request) => {
     const { searchParams } = new URL(req.url);
     const id = searchParams.get('id');
-
     if (!id) {
         return NextResponse.json({ error: 'Patient ID is required' }, { status: 400 });
     }
